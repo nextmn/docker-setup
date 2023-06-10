6ed1386111dce2876c3ef0a15d519c78861997d6
Louis Royer <infos.louis.royer@gmail.com>
2023-06-10 19:16:23 +0200

    Add licence headers

--- runtime/userhook.go
+++ runtime/userhook.go
@@ -1,3 +1,7 @@
+// Copyright 2023 Louis Royer and docker-setup contributors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+// SPDX-License-Identifier: MIT
 package setup
 
 import (
